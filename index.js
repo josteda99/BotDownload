77f683a5ee8a5bee88d10b5e9d553e2d031e28fd
josteda99 <josteda99@gmail.com>
2022-11-13 20:02:24 -0500

    feat: get id automatically

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 var YoutubeMp3Downloader = require("youtube-mp3-downloader");
-const lineReader = require('line-reader');
+const lineReader = require("line-reader");
 
-let id = []
+let id = [];
 
 var YD = new YoutubeMp3Downloader({
-  "ffmpegPath": "C:/ffmpeg-2022-03-28-git-5ee198f9aa-essentials_build/bin/ffmpeg.exe",
-  "outputPath": "C:/Users/USER/Documents/Code/BotDownload/music",
-  "youtubeVideoQuality": "highestaudio",
-  "queueParallelism": 2,
-  "progressTimeout": 2000,
-  "allowWebm": false
+  ffmpegPath:
+    "C:/ffmpeg-2022-03-28-git-5ee198f9aa-essentials_build/bin/ffmpeg.exe",
+  outputPath: "C:/Users/USER/Documents/Code/BotDownload/music",
+  youtubeVideoQuality: "highestaudio",
+  queueParallelism: 2,
+  progressTimeout: 2000,
+  allowWebm: false,
 });
 
-let cont = 0
-lineReader.eachLine('url_id.txt', function (line) {
-  console.log("cancion n : ", cont += 1)
-  YD.download(line);
+let cont = 0;
+lineReader.eachLine("url_id.txt", function (line) {
+  // get youtube id
+  console.log("song n : ", (cont += 1));
+  var video_id = line.split("v=")[1];
+  if (video_id) {
+    var ampersandPosition = video_id.indexOf("&");
+    if (ampersandPosition != -1) {
+      video_id = video_id.substring(0, ampersandPosition);
+    }
+    YD.download(video_id);
+  } else {
+    YD.download(line);
+  }
 
   YD.on("finished", function (err, data) {
     console.log(JSON.stringify(data));
   });
 
   YD.on("error", function (error) {
+    console.log(line, "video_id: ", video_id);
     console.log(error);
   });
 
---
+++ readme.md
@@ -0,0 +1,12 @@
+Hello :D
+
+Downloader By Youtube Link
+
+1. clone repository
+2. npm i
+3. copy the link in url_id.txt
+4. open console
+5. write: node index.js
+
+the music will be download in music folder 
+
